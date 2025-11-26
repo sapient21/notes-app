a61ec8d4ca0488b88e50f6c896a8e8a63a7ba688
leeq <sapientrules@gmail.com>
2025-11-26 08:38:09 +0300

    change UI

--- app/login.jsx
+++ app/login.jsx
@@ -5,41 +5,57 @@ import {
   TextInput,
   TouchableOpacity,
   StyleSheet,
+  Image,
 } from "react-native";
 
 export default function Login() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
   const [remember, setRemember] = useState(false);
 
   return (
     <View style={styles.container}>
-      
+      <Image
+        source={require("../assets/images/student-study-icon.png")}
+        style={styles.logoImg}
+      />
+
       <Text style={styles.title}>Login</Text>
       <View style={styles.underline} />
 
       
       <TextInput
         style={styles.input}
-        placeholder="name"
+        placeholder="Name"
         value={name}
         onChangeText={setName}
       />
 
       
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        secureTextEntry= {!show}
-        value={password}
-        onChangeText={setPassword}
-      />
+      <View style={styles.passwordWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={!show}
+          value={password}
+          onChangeText={setPassword}
+        />
+
+        <TouchableOpacity onPress={() => setShow(!show)} style={styles.eyeIcon}>
+          <Image
+            source={
+              show
+                ? require("../assets/images/my-notion-face-transparent (1).png")
+                : require("../assets/images/my-notion-face-transparent (2).png")
+            }
+            style={{ width: 50, height: 50 }}
+          />
+        </TouchableOpacity>
+      </View>
 
       
       <View style={styles.setRow}>
-
-
         <TouchableOpacity
           style={styles.setRowME}
           onPress={() => setRemember(!remember)}
@@ -49,33 +65,16 @@ export default function Login() {
           </View>
           <Text style={styles.rememberText}>remember me</Text>
         </TouchableOpacity>
-
-        
-        
-        
-        <TouchableOpacity
-          style={styles.setRow}
-          onPress={() => setShow(!show)}
-        >
-          <View style={styles.checkbox}>
-            {show && <View style={styles.checkboxInner} />}
-          </View>
-          <Text style={styles.rememberText}>Show Password</Text>
-        </TouchableOpacity>
-
-
       </View>
-
-
 
       
       <View style={styles.buttonRow}>
         <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>Login</Text>
+          <Text style={styles.loginText}>✔️Login</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.cancelBtn}>
-          <Text style={styles.cancelText}>Cancel</Text>
+          <Text style={styles.cancelText}>❌Cancel</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -106,12 +105,24 @@ const styles = StyleSheet.create({
   },
 
   input: {
-    borderWidth:2,
+    borderWidth: 2,
     borderColor: "#000",
-    padding: 12,
+    padding: 20,
+    paddingRight: 45, // space for the eye icon
     fontSize: 20,
     borderRadius: 8,
     marginBottom: 20,
+  },
+
+  passwordWrapper: {
+    position: "relative",
+    justifyContent: "center",
+  },
+
+  eyeIcon: {
+    position: "absolute",
+    right: 15,
+    top: 13,
   },
 
   setRow: {
@@ -123,8 +134,7 @@ const styles = StyleSheet.create({
   setRowME: {
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 30,
-    marginInline:23,
+    marginInline: 23,
   },
 
   checkbox: {
@@ -175,5 +185,14 @@ const styles = StyleSheet.create({
   cancelText: {
     color: "#000",
     fontSize: 18,
+  },
+
+  logoImg: {
+    width: 100,
+    height: 100,
+    alignSelf: "center",
+    borderWidth: 1,
+    borderRadius: 80,
+    padding: 80,
   },
 });
